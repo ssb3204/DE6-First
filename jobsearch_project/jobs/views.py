30fa06fcd9867c835dda662521d591149e65307b
‘h2k9971’ <h2k9971@naver.com>
2025-04-21 18:15:45 +0900

    feat: 기본 base 배경에 맞게 2차 수정

--- jobsearch_project/jobs/views.py
+++ jobsearch_project/jobs/views.py
@@ -11,12 +11,21 @@ def index(request):
 
 def job_list(request):
     job_list = JobPosting.objects.all().order_by('title')
-    paginator = Paginator(job_list, 50)
+    paginator = Paginator(job_list, 40)
     page_number = request.GET.get('page') # ?page=2 이런 URL에서 page 번호를 읽음
     page_obj= paginator.get_page(page_number)
     return render(request, 'jobs/job_list.html', {'page_obj': page_obj})
 
 def job_detail(request, pk):
     job = get_object_or_404(JobPosting, pk=pk)
-    return render(request, 'jobs/job_detail.html', {'job': job})
     
+    # 이전/다음 공고 구하기
+    prev_job = JobPosting.objects.filter(pk__lt=pk).order_by('-pk').first()
+    next_job = JobPosting.objects.filter(pk__gt=pk).order_by('pk').first()
+
+    return render(request, 'jobs/job_detail.html', {
+        'job': job,
+        'prev_job': prev_job,
+        'next_job': next_job
+    })
+
