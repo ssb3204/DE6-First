d406eae2b54a40ed5c0af56391f371203577cac1
Dong-yeong0 <kevin@beomhoit.com>
2025-04-22 13:12:19 +0900

    Add job post model

--- jobsearch_project/jobs/models.py
+++ jobsearch_project/jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class JobPosting(models.Model):
+    company = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    link = models.URLField()
+    description = models.TextField()
+    source = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.company} - {self.title} (출처: {self.source})'
