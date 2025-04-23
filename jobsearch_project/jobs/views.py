590731c68e993dcbe86c6e981e5921b438b4a3c0
ssb3204 <ryanp3@naver.com>
2025-04-23 13:04:58 +0900

    경력 +검색어 결과 추력력

--- jobsearch_project/jobs/views.py
+++ jobsearch_project/jobs/views.py
@@ -13,37 +13,41 @@ from django.core.paginator import Paginator
 def index(request):
     return render(request, 'jobs/index.html')
 
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+from django.db.models import Q
+from django.core.paginator import Paginator
+
 def search_page(request):
     query = request.GET.get('q', '')  # 검색어 받기
-    filter_by = request.GET.get('filter', 'company')  # 필터링 기준 (기본: 회사)
+    filter_by = request.GET.get('filter', 'company')  # 검색할 필드(기본값: company)
     selected_experience = request.GET.getlist('experience')  # 경력 사항 체크박스 필터 받기
 
     job_postings = JobPosting.objects.all()
 
-    # 검색어로 필터링
+    # 검색어로 필터링 (filter_by에 따라 필터링)
     if query:
-        if query in ['Java', 'Python', 'C++', 'C#', 'Ruby']:
-            korean = translate_to_korean(query)
-            job_postings = job_postings.filter(
-                Q(description__icontains=query) |
-                Q(description__icontains=korean)
-            )
+        if filter_by == 'company':
+            job_postings = job_postings.filter(company__icontains=query)
+        elif filter_by == 'title':
+            job_postings = job_postings.filter(title__icontains=query)
+        elif filter_by == 'description':
+            job_postings = job_postings.filter(description__icontains=query)
         else:
-            # 일반 검색
-            filter_kwargs = {f"{filter_by}__icontains": query}
-            job_postings = job_postings.filter(**filter_kwargs)
+            # 기본 필터링
+            job_postings = job_postings.filter(
+                Q(company__icontains=query) |
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
 
-    # 경력 사항 필터링
+    # 경력 사항 필터링 (experience_level 필드에서 필터링)
     if selected_experience:
-        experience_keywords = {
-            'junior': '경력 1 |신입',  # '신입' 또는 '경력 1년 이하'를 찾음
-            'mid': '경력 2 |경력 3',  # '경력 2년 이상', '경력 3년 이상'을 찾음
-            'senior': '경력 5|경력 6|경력 7|경력 8|경력 9',  # '경력 5년 이상'부터 '경력 9년 이상'까지 찾음
-        }
-        
-        # 선택한 경력 사항에 맞는 필터링 적용
-        experience_regex = '|'.join([experience_keywords[exp] for exp in selected_experience])
-        job_postings = job_postings.filter(description__regex=experience_regex)
+        job_postings = job_postings.filter(experience_level__in=selected_experience)
 
     # 페이지네이션
     paginator = Paginator(job_postings, 20)
@@ -56,6 +60,9 @@ def search_page(request):
         'filter_by': filter_by,
         'selected_experience': selected_experience,
     })
+
+
+
 
 def translate_to_korean(keyword):
     translation_dict = {
