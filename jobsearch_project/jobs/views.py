4beeb5df978ae48af58775fee0340a9c831febcb
ssb3204 <ryanp3@naver.com>
2025-04-23 11:26:21 +0900

    경력 구분 & 검색 결과 링크 추가가

--- jobsearch_project/jobs/views.py
+++ jobsearch_project/jobs/views.py
@@ -14,25 +14,39 @@ def index(request):
     return render(request, 'jobs/index.html')
 
 def search_page(request):
-    query = request.GET.get('q', '')
-    filter_by = request.GET.get('filter', 'company')
+    query = request.GET.get('q', '')  # 검색어 받기
+    filter_by = request.GET.get('filter', 'company')  # 필터링 기준 (기본: 회사)
+    selected_experience = request.GET.getlist('experience')  # 경력 사항 체크박스 필터 받기
 
+    job_postings = JobPosting.objects.all()
+
+    # 검색어로 필터링
     if query:
         if query in ['Java', 'Python', 'C++', 'C#', 'Ruby']:
             korean = translate_to_korean(query)
-            job_list = JobPosting.objects.filter(
+            job_postings = job_postings.filter(
                 Q(description__icontains=query) |
                 Q(description__icontains=korean)
             )
         else:
             # 일반 검색
             filter_kwargs = {f"{filter_by}__icontains": query}
-            job_list = JobPosting.objects.filter(**filter_kwargs)
-    else:
-        job_list = JobPosting.objects.all()
+            job_postings = job_postings.filter(**filter_kwargs)
+
+    # 경력 사항 필터링
+    if selected_experience:
+        experience_keywords = {
+            'junior': '경력 1 |신입',  # '신입' 또는 '경력 1년 이하'를 찾음
+            'mid': '경력 2 |경력 3',  # '경력 2년 이상', '경력 3년 이상'을 찾음
+            'senior': '경력 5|경력 6|경력 7|경력 8|경력 9',  # '경력 5년 이상'부터 '경력 9년 이상'까지 찾음
+        }
+        
+        # 선택한 경력 사항에 맞는 필터링 적용
+        experience_regex = '|'.join([experience_keywords[exp] for exp in selected_experience])
+        job_postings = job_postings.filter(description__regex=experience_regex)
 
     # 페이지네이션
-    paginator = Paginator(job_list, 20)
+    paginator = Paginator(job_postings, 20)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -40,6 +54,7 @@ def search_page(request):
         'page_obj': page_obj,
         'query': query,
         'filter_by': filter_by,
+        'selected_experience': selected_experience,
     })
 
 def translate_to_korean(keyword):
